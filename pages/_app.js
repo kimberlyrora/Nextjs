9842a00dfa95c8af64343542bac8a1431b9da526
Kimberly Rojas <kimberlyrojasra@gmail.com>
2022-01-12 01:21:07 -0500

    [add]: layout and styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+//just should be imported here!
+import '../styles/globals.css';
+import Layout from '../components/Layout';
 
+//Component: PageComponent
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout>
+	  <Component {...pageProps} />
+  </Layout>
 }
 
 export default MyApp
